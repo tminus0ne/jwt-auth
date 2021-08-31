991181a857d34a66591a07f82a11db0bd413d8b3
Greg-One <sgmdoc@yandex.ru>
2021-08-31 16:09:40 +0500

    add router and userControlle template

--- server/index.js
+++ server/index.js
@@ -4,12 +4,15 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 
+const router = require('./router/index');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
+app.use('/api', router);
 
 const start = async () => {
   try {
