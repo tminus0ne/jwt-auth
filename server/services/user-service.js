48c4a432a6850ee16a974a7dc8467ec6ff43be72
Greg-One <sgmdoc@yandex.ru>
2021-09-01 21:31:58 +0500

    add login functions

--- server/services/user-service.js
+++ server/services/user-service.js
@@ -45,6 +45,25 @@ class UserService {
     user.isActivated = true;
     await user.save();
   }
+
+  async login(email, password) {
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      throw ApiError.BadRequest(`User with email ${email} not found!`);
+    }
+
+    const isPassEquals = await bcrypt.compare(password, user.password);
+    if (!isPassEquals) {
+      throw ApiError.BadRequest('Incorrect password');
+    }
+
+    const userDto = new UserDto(user);
+    const tokens = tokenService.generateTokens({ ...userDto });
+
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+    return { ...tokens, user: userDto };
+  }
 }
 
 module.exports = new UserService();
