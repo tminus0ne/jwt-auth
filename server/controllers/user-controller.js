aaff0a7a281fe88fb079f5ee2c767b3f6a68f447
Greg-One <sgmdoc@yandex.ru>
2021-09-01 21:18:51 +0500

    add registration validation

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,8 +1,16 @@
+const { validationResult } = require('express-validator');
+
 const userService = require('../services/user-service');
+const ApiError = require('../exceptions/api-error');
 
 class UserController {
   async registration(req, res, next) {
     try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return next(ApiError.BadRequest('Validation error', errors.array()));
+      }
+
       const { email, password } = req.body;
       const userData = await userService.registration(email, password);
 
--- server/router/index.js
+++ server/router/index.js
@@ -1,10 +1,17 @@
 const Router = require('express').Router;
+const { body } = require('express-validator');
 
 const userController = require('../controllers/user-controller');
 
 const router = new Router();
 
-router.post('/registration', userController.registration);
+router.post(
+  '/registration',
+  body('email').isEmail(),
+  body('password').isLength({ min: 3, max: 32 }),
+  userController.registration,
+);
+
 router.post('/login', userController.login);
 router.post('/logout', userController.logout);
 router.get('/activate/:link', userController.activate);
