3f617575fd14f8a635fed3eaf878cbd051740c35
Greg-One <sgmdoc@yandex.ru>
2021-09-02 14:29:15 +0500

    add store & context

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App';
+
+import { Store } from './store/store';
+
+interface State {
+  store: Store;
+}
+
+const store = new Store();
+
+export const Context = createContext<State>({
+  store,
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Context.Provider
+      value={{
+        store,
+      }}
+    >
+      <App />
+    </Context.Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
